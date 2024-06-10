c0b88975f30bda9980c002a29f982234a5223520
youngandmad <kkraken2005@gmail.com>
2024-06-10 19:28:28 +0600

    no tests

--- src/test/kotlin/kz/danekerscode/coassembleapi/CoAssembleApiApplicationTests.kt
+++ src/test/kotlin/kz/danekerscode/coassembleapi/CoAssembleApiApplicationTests.kt
@@ -1,13 +1,8 @@
 package kz.danekerscode.coassembleapi
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
 class CoAssembleApiApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    fun contextLoads() {
+    }
 
 }
